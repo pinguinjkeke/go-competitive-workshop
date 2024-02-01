6c4e18f919f813c412d4f670f100e6ae3b57bb48
vladislav.poklonski <poklonski.vladislav@gmail.com>
2024-02-01 20:21:54 +0100

    add the argument handler

--- src/cmd/root.go
+++ src/cmd/root.go
@@ -10,16 +10,58 @@ func RootCommand() *cobra.Command {
 		Short: "A basic CLI example",
 		Long:  "A basic CLI example using Cobra",
 		Run: func(cmd *cobra.Command, args []string) {
+		},
+	}
+
+	pokemonCommand := &cobra.Command{
+		Use:   "pokemon",
+		Short: "",
+		Long:  "",
+		Run: func(cmd *cobra.Command, args []string) {
 			client := NewPokemonApiClient()
 
-			client.FindPokemon("bulbasaur")
+			if len(args) < 1 {
+				cmd.Println("Please enter a pokemon name")
+			}
 
-			cmd.Println("Welcome to FlyPoke!!")
+			if args[0] == "pokemon" {
+				client.FindPokemon(args[0])
+			}
+		},
+	}
+
+	attackCommand := &cobra.Command{
+		Use:   "attack",
+		Short: "",
+		Long:  "",
+		Run: func(cmd *cobra.Command, args []string) {
+			client := NewPokemonApiClient()
+
+			if args[0] == "pokemon" {
+				client.FindPokemon(args[0])
+			}
+		},
+	}
+
+	statsCommand := &cobra.Command{
+		Use:   "stats",
+		Short: "",
+		Long:  "",
+		Run: func(cmd *cobra.Command, args []string) {
+			client := NewPokemonApiClient()
+
+			if args[0] == "pokemon" {
+				client.FindPokemon(args[0])
+			}
 		},
 	}
 
 	// Register your commands here
 	// cmd.AddCommand(ExampleCommand)
+
+	cmd.AddCommand(pokemonCommand)
+	cmd.AddCommand(attackCommand)
+	cmd.AddCommand(statsCommand)
 
 	return cmd
 }
